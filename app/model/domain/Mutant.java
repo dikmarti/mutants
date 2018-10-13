9f52818da4c07774294fceea7d1c28c2b9aa4ab0
Diana Martinez <dikmarti@gmail.com>
2018-10-12 22:15:24 -0300

    Commit inicial

---
+++ app/model/domain/Mutant.java
@@ -0,0 +1,22 @@
+package model.domain;
+
+public class Mutant {
+	
+	private String[] adn;
+	
+	public Mutant(String[] adn) {
+		super();
+		this.adn = adn;
+	}
+
+	public String[] getAdn() {
+		return adn;
+	}
+
+	public void setAdn(String[] adn) {
+		this.adn = adn;
+	}
+	
+	
+
+}
